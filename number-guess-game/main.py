a00044e8937ba405f67195e8016300038423eb74
Pratik Dahal <pratikd354@gmail.com>
2026-01-04 22:33:11 +0545

    with use of functions and try except block

--- number-guess-game/main.py
+++ number-guess-game/main.py
@@ -1,22 +1,43 @@
 
+# import random
+
+# random_num = random.randint(1,100)
+
+# while True:
+#     try:
+
+#         user_num = int(input("Enter a number between 1 to 100: "))
+
+#         if random_num == user_num:
+#             print("Yayy! you guessed it right masterrr.")
+#             break
+#         elif random_num > user_num:
+#             print("Too low!")
+        
+#         else:
+#             print("Too high!")
+#     except ValueError:
+#         print("Invalid choice")
+
 import random
 
 random_num = random.randint(1,100)
 
+def user_num():
+    num = int(input("Enter a number between 1 to 100: "))
+    return num
+
 while True:
     try:
-
-        user_num = int(input("Enter a number between 1 to 100: "))
-
-        if random_num == user_num:
-            print("Yayy! you guessed it right masterrr.")
+        user_guess = user_num()
+        if random_num == user_guess:
+            print("Yayy! You guessed it master.")
             break
-        elif random_num > user_num:
+        elif random_num > user_guess:
             print("Too low!")
         
         else:
             print("Too high!")
+
     except ValueError:
-        print("Invalid choice")
-
-
+        print("Invalid Choice!!!")
