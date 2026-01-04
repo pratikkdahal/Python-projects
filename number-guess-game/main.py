3e9b3939d405c17be3d409d5c38cce56d50168f3
Pratik Dahal <pratikd354@gmail.com>
2026-01-04 22:14:37 +0545

    with try except block

--- number-guess-game/main.py
+++ number-guess-game/main.py
@@ -1,31 +1,22 @@
-# random import
-#infinite loop 
-    #user sanga number magnu paryo
-    #random bata aako number ra user ko number compare
-    #if same print yayy you guessed
-    # if different you faile
-    # if invalid show invalid input
 
 import random
 
-def get_number():
-    num = random.randint(1,100)
-    print(num)
-
-def get_usernum():
-    user_num = int(input("Enter a number from 1 to 100: "))
-    return get_usernum
+random_num = random.randint(1,100)
 
 while True:
+    try:
 
-    user_num = get_usernum()
-    
-    if get_number == get_usernum:
-        print("Yayy ! You guessed it right.")
-    elif get_number != get_usernum:
-        print("You cannot guess, sorry!")
-        break
-    else:
-        print("Invalid number,please enter valid one!")
+        user_num = int(input("Enter a number between 1 to 100: "))
+
+        if random_num == user_num:
+            print("Yayy! you guessed it right masterrr.")
+            break
+        elif random_num > user_num:
+            print("Too low!")
+        
+        else:
+            print("Too high!")
+    except ValueError:
+        print("Invalid choice")
 
 
